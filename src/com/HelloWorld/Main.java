6175958ffb06e9c6ba877d61af0e34cdebfa67c3
Wyatt Enourato <wenourato@gmail.com>
2022-01-24 15:49:49 -0700

    Working with some simple primitive types

---
+++ src/com/HelloWorld/Main.java
@@ -0,0 +1,13 @@
+package com.HelloWorld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte age = 36;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isElegiable = false;
+        System.out.println(viewsCount);
+    }
+}
