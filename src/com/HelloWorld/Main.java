610d1114b4058a6141136c4ccf586d7f2dec6c88
Wyatt Enourato <wenourato@gmail.com>
2022-01-24 18:18:16 -0700

    Working with some simple primitive types

--- src/com/HelloWorld/Main.java
+++ src/com/HelloWorld/Main.java
@@ -1,16 +1,30 @@
 package com.HelloWorld;
 
-import java.util.Date;
+//import java.util.Date;
+
+//import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        byte age = 36;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isElegiable = false;
+//        byte age = 36;
+//        long viewsCount = 3_123_456_789L;
+//        float price = 10.99F;
+//        char letter = 'A';
+//        boolean isElegiable = false;
 //        Date now = new Date();
-        System.out.println(price);
+
+//        byte x = 1;
+//        byte y = x;
+//        x = 2;
+
+//        Point point1 = new Point(1,1);
+//        Point point2 = point1;
+//        point1.x = 2;
+
+            String message = "Hello World" + "!!";
+
+
+        System.out.println(message.replace("!", "?"));
     }
 }
